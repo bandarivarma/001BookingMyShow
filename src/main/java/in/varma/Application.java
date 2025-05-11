2e1b74a59754c596dbd58cb36c6c0e086144c040
Bandari verma <140159081+bandarivarma@users.noreply.github.com>
2025-05-11 13:00:27 +0530

    Update Application

--- src/main/java/in/varma/Application.java
+++ src/main/java/in/varma/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		system.out.println("vyshnavi");
+		system.out.println("varma");
 	}
 
 }
